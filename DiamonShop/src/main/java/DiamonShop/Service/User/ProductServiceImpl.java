cdaff79759817dca460ca646972f9356d6c9d658
HuynhVanThuy <huynhvanthuy97@gmail.com>
2020-05-26 22:37:12 +0700

    Add Bai22

---
+++ DiamonShop/src/main/java/DiamonShop/Service/User/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package DiamonShop.Service.User;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import DiamonShop.Dao.ProductsDao;
+import DiamonShop.Dto.ProductsDto;
+@Service
+public class ProductServiceImpl implements IProductService {
+	
+	@Autowired
+	ProductsDao productDao = new ProductsDao();
+
+	public ProductsDto GetProductByID(long id) {
+		List<ProductsDto> listProducts = productDao.GetProductByID(id);
+		return listProducts.get(0);
+	}
+
+}
